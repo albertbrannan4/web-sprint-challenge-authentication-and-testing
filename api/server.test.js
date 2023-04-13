0886fb15f98d89c1ca25366f9741ce3ff7c2d39f
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-13 12:33:26 -0400

    register tests wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,34 @@
+const db = require("../data/dbConfig");
+const request = require("supertest");
+const server = require("./server");
+const bcrypt = require("bcryptjs");
+const e = require("express");
+beforeEach(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).not.toBe(false);
+});
+
+describe("[POST]  api/auth/register", () => {
+  const newUser = { username: "foo", password: "bar" };
+  test("register creates new user in the database", async () => {
+    await request(server).post("/api/auth/register").send(newUser);
+    expect(await db("users")).toHaveLength(1);
+  });
+  test("register responds with 201", async () => {
+    const res = await request(server).post("/api/auth/register").send(newUser);
+    expect(res.status).toBe(201);
+  });
+
+  test("register responds with username and password", async () => {
+    const res = await request(server).post("/api/auth/register").send(newUser);
+    expect(res.body).toMatchObject({ username: "foo" });
+    expect(res.body.password !== undefined).toBe(true);
+  });
+
+  test.todo("register uses bcrypt to authenticate the password");
+});
